959795b0aa25a30a963469d209f27562ed2e9d74
8bitbran <brandonmar97@gmail.com>
2020-04-18 15:25:09 -0400

    Descriptions are instantiated with API response

--- lib/trees-cli/descriptions.rb
+++ lib/trees-cli/descriptions.rb
@@ -1,5 +1,28 @@
 class TreesCLI::Descriptions
 
+    @@all = [] # holds all descriptions
 
+    def self.all # returns array of all descriptions objects
+        @@all
+    end
+    
+    def self.create_from_api(id, description)
+       self.create(id, description)
+    end 
+
+    def self.create(id, description)
+        new(id, description).save
+    end 
+
+    attr_reader :id, :text
+
+    def initialize(id, description)
+        @id = id
+        @text = ["desc"]
+    end
+
+    def save 
+        @@all << self 
+    end 
 
 end
